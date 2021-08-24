6c87e962da9e8afa07132ff63d7824348e26d890
vivek sharma <vivekslim@gmail.com>
2021-08-24 13:30:41 +0530

    update assignment

--- block-BRaaas/my-app/src/components/Articles.js
+++ block-BRaaas/my-app/src/components/Articles.js
@@ -1,4 +1,10 @@
 //Article component
+import names from "./data/names";
+
+function handleClick(value , event) {
+    alert(`hello ${value} , How are you!! :)`);
+    console.log(event)
+}
 
 function Articles() {
     return (
@@ -6,9 +12,12 @@ function Articles() {
         <section className="news padding">
           <h3 className="heading-secondary">Our latest News</h3>
           <div className="container flex justify-between align-center wrap">
-            <Article heading="How to build a great portfolio as beginner developer" />
-            <Article heading="How to build a great portfolio as beginner developer" />
-            <Article heading="How to build a great portfolio as beginner developer" />
+           
+
+            {names.map((name) => (
+            <Article  {...name} key={name.id} />
+          ))}
+            
           </div>
         </section>
       </>
@@ -23,12 +32,20 @@ function Articles() {
           src="https://static.coindesk.com/wp-content/uploads/2020/05/btc-chart-may-12-1200x628.png"
           alt="news"
         />
-        <h3>{props.heading}</h3>
+        <h3>{props.value}</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempore
           ducimus laudantium fugiat facilis? Beatae deleniti quisquam reprehen.
         </p>
-        <a href="#">Read More</a>
+        
+
+        <button
+          onClick={(event) => handleClick(props.value, event)}
+          className="btn"
+        >
+          click {props.value}
+        </button>
+
       </article>
     );
   }
---
+++ block-BRaaas/my-app/src/components/data/names.js
@@ -0,0 +1,8 @@
+let names = [
+    { id: 'apple', value: '🍎 Arya' },
+    { id: 'orange', value: '🍊 John' },
+    { id: 'grape', value: '🍇 Vivek' },
+    { id: 'pear', value: '🍐 Nitin' },
+  ];
+
+export default names
